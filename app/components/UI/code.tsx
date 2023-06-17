32bb2cf6968e5ca77eca54da7bc6b4e47e96e53b
nicolasito1411 <60229704+Marchand-Nicolas@users.noreply.github.com>
2023-06-17 15:40:37 +0200

    fixing build errors

--- app/components/UI/code.tsx
+++ app/components/UI/code.tsx
@@ -1,8 +1,7 @@
-import styles from "@/app/styles/components/UI/code.module.css";
 import SyntaxHighlighter from "react-syntax-highlighter";
 import { anOldHope } from "react-syntax-highlighter/dist/esm/styles/hljs";
 
-const Code = ({ children }: { children: React.ReactNode }) => {
+const Code = ({ children }: { children: string }) => {
   return (
     <SyntaxHighlighter language={"json"} style={anOldHope}>
       {children}
